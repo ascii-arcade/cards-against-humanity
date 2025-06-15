d5b3e73a6bb615d9fc80fadc3170d4d58971eb04
Keith Thibodeaux <keith@railscoder.net>
2025-06-15 16:01:36 -0400

    remove markdown rendering

--- deck/cards.go
+++ deck/cards.go
@@ -1,28 +1,5 @@
 package deck
 
-import (
-	"strings"
-
-	"github.com/charmbracelet/glamour"
-)
-
-const style = `
-{
-  "document": {
-    "margin": 0,
-    "padding": 0
-  },
-  "paragraph": {
-    "margin": 0,
-    "padding": 0
-  }
-}
-`
-
-var renderer, _ = glamour.NewTermRenderer(
-	glamour.WithStylesFromJSONBytes([]byte(style)),
-)
-
 type AnswerCard struct {
 	ID         int
 	IsRevealed bool
@@ -34,20 +11,4 @@ type QuestionCard struct {
 	IsRevealed bool
 	Text       string `json:"text"`
 	Pick       int    `json:"pick"`
-}
-
-func (card *AnswerCard) String() string {
-	return markdownRender(card.Text)
-}
-
-func (card *QuestionCard) String() string {
-	return markdownRender(card.Text)
-}
-
-func markdownRender(text string) string {
-	rendered, err := renderer.Render(text)
-	if err != nil {
-		return text
-	}
-	return strings.TrimSpace(rendered)
 }
